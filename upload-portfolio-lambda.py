7640735ab8f55aa70383ac0d1c07d2540ca5a6ca
Kayla Webb <kaylarwebb@gmail.com>
2019-03-16 15:23:03 -0400

    feat: write lambda to upload site to s3 bucket

---
+++ upload-portfolio-lambda.py
@@ -0,0 +1,22 @@
+import boto3
+import StringIO
+import zipfile
+import mimetypes
+
+# setup s3 resource
+s3 = boto3.resource('s3')
+# get portfolio bucket and the build 
+portfolio_bucket = s3.Bucket('portfolio.kaylarwebb')
+build_bucket = s3.Bucket('portfoliobuild.kaylarwebb')
+
+# create in memory file
+portfolio_zip = StringIO.StringIO()
+# download bucket object from s3 and set it to in memory file
+build_bucket.download_fileobj('webbportfolio.zip', portfolio_zip)
+
+# extract, upload and set the acl 
+with zipfile.ZipFile(portfolio_zip) as myzip:
+    for nm in myzip.namelist():
+        obj = myzip.open(nm)
+        portfolio_bucket.upload_fileobj(obj, nm)
+        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
