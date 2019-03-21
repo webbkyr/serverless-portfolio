9a0382d3042fc1eb30752383f54d44be2f448c5f
Kayla Webb <kaylarwebb@gmail.com>
2019-03-21 18:18:52 -0400

    feat: update portfolio build fn for lambda

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -1,22 +1,30 @@
+import json
 import boto3
 import StringIO
 import zipfile
 import mimetypes
 
-# setup s3 resource
-s3 = boto3.resource('s3')
-# get portfolio bucket and the build 
-portfolio_bucket = s3.Bucket('portfolio.kaylarwebb')
-build_bucket = s3.Bucket('portfoliobuild.kaylarwebb')
+def lambda_handler(event, context):
 
-# create in memory file
-portfolio_zip = StringIO.StringIO()
-# download bucket object from s3 and set it to in memory file
-build_bucket.download_fileobj('webbportfolio.zip', portfolio_zip)
+    # setup s3 resource
+    s3 = boto3.resource('s3')
+    # get portfolio bucket and the build
+    portfolio_bucket = s3.Bucket('portfolio.kaylarwebb')
+    build_bucket = s3.Bucket('portfoliobuild.kaylarwebb')
 
-# extract, upload and set the acl 
-with zipfile.ZipFile(portfolio_zip) as myzip:
-    for nm in myzip.namelist():
-        obj = myzip.open(nm)
-        portfolio_bucket.upload_fileobj(obj, nm)
-        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+    # create in memory file
+    portfolio_zip = StringIO.StringIO()
+    # download bucket object from s3 and set it to in memory file
+    build_bucket.download_fileobj('webbportfolio.zip', portfolio_zip)
+
+    # extract, upload and set the acl
+    with zipfile.ZipFile(portfolio_zip) as myzip:
+        for nm in myzip.namelist():
+            obj = myzip.open(nm)
+            portfolio_bucket.upload_fileobj(obj, nm)
+            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+
+    return {
+    'statusCode': 200,
+    'body': json.dumps('Hello from Lambda!')
+    }
